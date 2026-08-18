38f872707a7aeb3b7a0a36ccf7a2b0934240446a
vikkont79 <vvikkont79@yandex.ru>
2026-08-18 17:55:49 +0300

    refactor: меняет принцип блокировки с аккаунтной на per-IP

---
+++ db/migrations/0009_abnormal_viper.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `users` DROP COLUMN `failed_login_attempts`;--> statement-breakpoint
+ALTER TABLE `users` DROP COLUMN `locked_until`;--> statement-breakpoint
+ALTER TABLE `users` DROP COLUMN `last_failed_at`;
